30d8257ac4f4e77c25e47d2cf13b3d8233098f38
Badusha <113815355+badusha01@users.noreply.github.com>
2023-06-16 16:04:52 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -30,15 +30,12 @@ const store = configureStore({
   })
 })
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistStore(store)}>
       <App />
-
       </PersistGate>
       </Provider>
   </React.StrictMode>
